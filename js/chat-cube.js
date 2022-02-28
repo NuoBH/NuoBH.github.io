36ee9bd663f56a1668a34325dcc72ecbfcc69111
NuoBH <36141903+NuoBH@users.noreply.github.com>
2022-02-28 15:38:45 -0800

    update bug of face not scroll/click
    delete chat cube face overflow:none

--- js/chat-cube.js
+++ js/chat-cube.js
@@ -89,14 +89,15 @@ class ChatCube{
         };
 
 
-        // this.checkBlur();
-        this.startTilt();
+        this.checkBlur();
+        // this.startTilt();
 
         this.cubeOnResize();
         window.addEventListener(`resize`, this.cubeOnResize.bind(this));
 
 
-        this.front.style.setProperty(`z-index`, `2`);
+        // this.front.style.setProperty(`z-index`, `2`);
+        this.front.style.setProperty(`pointer-events`, `all`);
 
     }
 
@@ -259,11 +260,14 @@ class ChatCube{
     }
 
     hideAndShow(toRemove, toAdd){
-        toRemove.style.setProperty(`z-index`, `initial`);
-        toAdd.style.setProperty(`z-index`, `2`);
+        // toRemove.style.setProperty(`z-index`, `initial`);
+        // toAdd.style.setProperty(`z-index`, `2`);
 
         toRemove.children[0].style.setProperty(`opacity`, `0`);
         toAdd.children[0].style.setProperty(`opacity`, `1`);
+
+        toRemove.style.setProperty(`pointer-events`, `none`);
+        toAdd.style.setProperty(`pointer-events`, `all`);
 
         let videos = toRemove.firstElementChild.querySelectorAll("video");
         videos.forEach(function(curVideo){
@@ -339,11 +343,11 @@ class ChatCube{
         }
 
         let textDOM = face.firstElementChild;
-        // if(isSafari){
-        //     textDOM.addEventListener(`wheel`, function(e){
-        //         if(this.curstate == allow) this.safariScroll(e, face);
-        //     }.bind(this));
-        // }
+        if(isSafari){
+            textDOM.addEventListener(`wheel`, function(e){
+                if(this.curstate == allow) this.safariScroll(e, face);
+            }.bind(this));
+        }
 
         if(mobileAndTabletCheck()){
             textDOM.addEventListener(`touchstart`, function(e){
@@ -354,11 +358,7 @@ class ChatCube{
                 if(this.curstate == allow && canAddScroll) this.mobileScroll(e, face);  
             }.bind(this));
         }
-        else{
-            textDOM.addEventListener(`wheel`, function(e){
-                if(this.curstate == allow) this.safariScroll(e, face);
-            }.bind(this));
-        }
+
     }
 
 
