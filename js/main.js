40d1f72a1f7a2507c28634b078399935404c4500
NuoBH <36141903+NuoBH@users.noreply.github.com>
2024-02-01 01:56:02 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ window.addEventListener(`firstCollide`, function(){
         let optionDOM = optionCreator.addOptionButtons(frontFace, 
                         [next]);
             
-        addEventHandlerToButtons(optionDOM, optionEventName, waterOption0);
+        addEventHandlerToButtons(optionDOM, optionEventName, waterEnd);
     }, 2500);
     
 });
